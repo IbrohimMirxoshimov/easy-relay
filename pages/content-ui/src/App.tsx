a5d966f4957ced7de47f9acab8ea2032e72ad044
Ibrohim <imirxoshimov@gmail.com>
2024-12-30 23:47:05 +0500

    fix: adjust padding for active tab body in utility bar style

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -96,7 +96,7 @@ const audio = new Audio(chrome.runtime.getURL('content-ui/sound-1.wav'));
  */
 function insertStyleTag() {
   const style = document.createElement('style');
-  style.innerHTML = `#utility-bar { bottom: 130px !important; }`;
+  style.innerHTML = `#utility-bar { bottom: 130px !important; }\n#active-tab-body { padding-bottom: 200px; }`;
   document.head.appendChild(style);
 }
 
